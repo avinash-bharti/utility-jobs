bbb677f0664583e0d3093f1a0755da824e032311
Avinash Bharti <90600575+avinash-bharti@users.noreply.github.com>
2023-10-11 18:12:26 +0530

    Update execute_xcuitest_build_every_5_min.rb

--- execute_xcuitest_build_every_5_min.rb
+++ execute_xcuitest_build_every_5_min.rb
@@ -17,18 +17,20 @@ url = URI.parse('https://api-cloud.browserstack.com/app-automate/xcuitest/v2/bui
 
 # app ids
 apps = {
-  "sampleApp" => {
-    "main" => "bs://9f664a3af2d5240b5dc1076934d33b2e17aa45db",
-    "test" => "bs://e868792f81617c1eac71faa58004ad2c16a1bd1a"
+  "budgetKeeper" => {
+    "main" => "bs://e36cfabf495f7b4a4c6aff8a80b3a2795c42521c",
+    "test" => "bs://91d13dc6c50e2387b72793ee5293d5643a5ac19d"
   }
 }
 
+hosts = ["203.32.41.137", "203.32.41.138"]
+
 # Create a hash for the request data
 data = {
-  "devices" => ["iPhone 12-17 beta", "iPhone 13-17 beta", "iPhone 12 Pro-17 Beta"],
-  "project" => "ios 17 5",
+  "devices" => ["iPhone 15-17.0"],
+  "project" => "ios 17",
   "build" => "github actions",
-  "buildTag" => "ios 17 issue reappear",
+  "buildTag" => "ios 17 workaround",
   "networkLogs" => "true",
   "deviceLogs" => "true",
   "enableResultBundle" => "true"
@@ -40,29 +42,32 @@ num_runs.times do |run_number|
   puts "Run Number: #{run_number + 1}"
 
   apps.each do |key, value|
-    # Create an HTTP POST request
-    request = Net::HTTP::Post.new(url)
-    request['Content-Type'] = 'application/json'
-
-    data["app"] = value["main"]
-    data["testSuite"] = value["test"]
-    request.body = data.to_json
-
-    # Encode the credentials in Base64
-    auth_token = Base64.strict_encode64("#{username}:#{access_key}")
-
-    # Set the Authorization header
-    request['Authorization'] = "Basic #{auth_token}"
-
-    # Make the request
-    response = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
-      http.request(request)
+    hosts.each do |host|
+      # Create an HTTP POST request
+      request = Net::HTTP::Post.new(url)
+      request['Content-Type'] = 'application/json'
+  
+      data["app"] = value["main"]
+      data["testSuite"] = value["test"]
+      data["machine"] = host
+      request.body = data.to_json
+  
+      # Encode the credentials in Base64
+      auth_token = Base64.strict_encode64("#{username}:#{access_key}")
+  
+      # Set the Authorization header
+      request['Authorization'] = "Basic #{auth_token}"
+  
+      # Make the request
+      response = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
+        http.request(request)
+      end
+  
+      # Print the response
+      puts "Response Code: #{response.code}"
+      puts "Response Body: #{response.body}"
+      sleep (2)
     end
-
-    # Print the response
-    puts "Response Code: #{response.code}"
-    puts "Response Body: #{response.body}"
-    sleep (60)
   end
 
   # Sleep for 5 minutes (300 seconds) between runs
